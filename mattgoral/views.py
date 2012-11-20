6f956e83887aaae03e63846dae2ee5f8f8138684
OpenShift guest <mtgoral@gmail.com>
2012-11-20 13:19:23 -0500

    asdfasdf

--- mattgoral/views.py
+++ mattgoral/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def home(request) :
-    return render_to_response("base.html")
+    return render_to_response("index.html")
