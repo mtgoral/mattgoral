88a10e1923f2673b9e6a1c0ad0eeaacd61e10c27
OpenShift guest <mtgoral@gmail.com>
2012-11-20 01:41:49 -0500

    space?

--- mattgoral/urls.py
+++ mattgoral/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-   url(r'^$', 'mattgoral.views.home', name='home'),
-   # url(r'^mattgoral/', include('mattgoral.foo.urls')),
+    url(r'^$', 'mattgoral.views.home', name='home'),
+    # url(r'^mattgoral/', include('mattgoral.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
