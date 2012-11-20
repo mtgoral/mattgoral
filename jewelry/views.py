b4f397c0f7e1f28238a104550c9773b5f5d6316d
OpenShift guest <mtgoral@gmail.com>
2012-11-20 01:07:28 -0500

    awesome

--- jewelry/views.py
+++ jewelry/views.py
@@ -1,4 +1,11 @@
+from django.template import Context, loader
+from jewelry.models import Jewelry
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hi")
+    jewelry_list = Jewelry.objects.all().order_by('id')[:5]
+    t = loader.get_template('jewelry/index.html')
+    c = Context({
+        'jewelry_list': jewelry_list,
+    })
+    return HttpResponse(t.render(c))
