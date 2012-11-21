af6f50df022f5b8942cbd006fdf99f5638bbbb7f
OpenShift guest <mtgoral@gmail.com>
2012-11-20 20:55:58 -0500

    try this shit

--- jewelry/views.py
+++ jewelry/views.py
@@ -2,6 +2,7 @@ from django.template import Context, loader
 from jewelry.models import Jewelry
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.http import Http404
 
 def index(request):
     jewelry_list = Jewelry.objects.all().order_by('id')[:5]
