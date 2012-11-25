4880521a6fce6a866a09c18afdbb164a9a73d562
OpenShift guest <mtgoral@gmail.com>
2012-11-25 18:51:43 -0500

    asdfdsa

--- bar/admin.py
+++ bar/admin.py
@@ -1,5 +1,7 @@
-from bar.models import Bar, Drink
+from bar.models import Bar, Drink, Order, OrderItem
 from django.contrib import admin
 
 admin.site.register(Bar)
 admin.site.register(Drink)
+admin.site.register(Order)
+admin.site.register(OrderItem)
