e1164a3f703ea799615a52d0a14018f015f703fc
OpenShift guest <mtgoral@gmail.com>
2012-11-25 15:23:27 -0500

    return self

--- bar/models.py
+++ bar/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 class Bar(models.Model):
     name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 
 class Drink(models.Model):
     bar = models.ForeignKey(Bar)
     name = models.CharField(max_length=200)
     price = models.CharField(max_length=20)
+    def __unicode__(self):
+        return self.name
