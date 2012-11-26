54f862f28e4ff4244583e1abb84c3db0e2e65037
OpenShift guest <mtgoral@gmail.com>
2012-11-25 20:37:09 -0500

    asdfdsaf

--- bar/views.py
+++ bar/views.py
@@ -1,7 +1,7 @@
 from django.template import Context, loader
 from bar.models import *
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.http import Http404
 from django.template import RequestContext
 
