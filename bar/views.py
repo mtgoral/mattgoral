03a5566f904696f6efac16b8e2f55214bbb63c31
OpenShift guest <mtgoral@gmail.com>
2012-11-25 21:15:44 -0500

    fuck

--- bar/views.py
+++ bar/views.py
@@ -25,8 +25,7 @@ def drink(request, drink_id):
     if request.method == 'POST':
         form = OrderForm(request.POST, request.FILES)
         if form.is_valid():
-            qqq = request.POST['order']
-            order = Order.objects.get(pk=qqq)
+            order = Order.objects.get(pk=request.POST['order'])
             drink = request.POST['drink']
             quantity = request.POST['quantity']
             orderitem = OrderItem(order=order, drink=drink, quantity=quantity)
